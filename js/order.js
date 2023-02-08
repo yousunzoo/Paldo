39b559254c9472c56b468aa45c95eb067b806bf8
bongsee <ajinsuk3@naver.com>
2023-02-08 14:58:52 +0900

    :sparkles: 주문서 페이지 퍼블리싱
    - 쿠폰 /  적립금 영역

--- js/order.js
+++ js/order.js
@@ -2,28 +2,31 @@
 const $ = selector => document.querySelector(selector)
 
 /* DOM */
-const showTogglerEl = $('#showToggler');
-console.log(showTogglerEl)
+const toggleOrderListEl = $('#toggleOrderList');
+const toggleCouponListEl = $('#toggleCouponList')
 
-showTogglerEl.addEventListener('click', toggleOrderList())
+
+/* EVENT LISTENER */
+toggleOrderListEl.addEventListener('click', toggleOrderList())
+toggleCouponListEl.addEventListener('click', toggleCouponList)
 
 function toggleOrderList() {
   let isActive = false;
   return function (event) {
     const btnEl = event.currentTarget;
-    const ulEl = $('.area-order-list > ul')
-    const summaryEl = $('.area-order-list > .summary')
+    const ulEl = $('.order-list > ul')
+    const summaryEl = $('.order-list > .summary')
     if(!isActive) {
-      ulEl.innerHTML = "활성화 상태입니다."
+      ulEl.innerHTML = '활성화 상태입니다.'
       btnEl.alt = "펼치기"
-      btnEl.style.transform = "rotateX(180deg)"
-      summaryEl.style.display = "none"
+      btnEl.style.transform = 'rotateX(180deg)'
+      summaryEl.style.display = 'none'
     }
     if(isActive) {
       ulEl.innerHTML = '';
       btnEl.alt = "펼치기"
-      btnEl.style.transform = "rotateX(0)"
-      summaryEl.style.display = "block"
+      btnEl.style.transform = 'rotateX(0)'
+      summaryEl.style.display = 'block'
     }
     isActive = !isActive;
 
@@ -31,4 +34,9 @@ function toggleOrderList() {
     // DOM Create !
     // -> Render DOM Element !
   }
+}
+
+function toggleCouponList() {
+  const couponListEl = $('.coupon-list')
+  couponListEl.classList.toggle('opened')
 }
