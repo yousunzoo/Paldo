bd4e2541bad016cbc0ef4b64752ca2287d94d25c
bongsee <ajinsuk3@naver.com>
2023-02-10 13:28:37 +0900

    :sparkles: 주문 상품 섹션 토글 버튼 클릭 시 상품 리스트 출력 기능 추가
    - localStorage에 임의의 mockup data를 넣어 기능 구현, 추후 장바구니 기능과 연동 예정

--- js/order-sheet/order-sheet.js
+++ js/order-sheet/order-sheet.js
@@ -1,42 +1,96 @@
+// import { makeDOMwithProperties } from "../utils/dom.js"
+
+import { makeDOMwithProperties } from '../utils/dom';
+
 /* COMMON */
 const $ = selector => document.querySelector(selector)
 
 /* DOM */
 const toggleOrderListEl = $('#toggleOrderList');
-const toggleCouponListEl = $('#toggleCouponList')
+const toggleCouponListEl = $('#toggleCouponList');
 
 
 /* EVENT LISTENER */
-toggleOrderListEl.addEventListener('click', toggleOrderList())
-toggleCouponListEl.addEventListener('click', toggleCouponList)
+toggleOrderListEl.addEventListener('click', toggleOrderList());
+toggleCouponListEl.addEventListener('click', toggleCouponList);
+
+/* GLOBAL LOGIC */
+setMockData();
+
+
+
+
+
+/* FUNCTIONS */
+
+/**
+ * localStorage Mock Data Setting
+ */
+function setMockData () {
+  // 제품명, 가격, 수량, 썸네일이미지
+  const cartProduct = [
+    { productId : 1, title : "감자깡", price : 7000, quantity : 1, thumbnailImage : "/images/product/감자깡.png" },
+    { productId : 2, title : "감자면", price : 6000, quantity : 1, thumbnailImage : "/images/product/감자면.png" },
+    { productId : 3, title : "고구마깡", price : 5000, quantity : 1, thumbnailImage : "/images/product/고구마깡.png" },
+    { productId : 4, title : "둥지냉면동치미물냉면", price : 4000, quantity : 1, thumbnailImage : "/images/product/둥지냉면동치미물냉면.png" },
+  ]
+  localStorage.setItem('cartProduct', JSON.stringify(cartProduct))
+}
 
 function toggleOrderList() {
   let isActive = false;
   return function (event) {
     const btnEl = event.currentTarget;
-    const ulEl = $('.order-list-area > ul')
-    const summaryEl = $('.order-list-area > .summary')
+    const ulEl = $('.order-list-area > ul');
+    const summaryEl = $('.order-list-area > .summary');
     if(!isActive) {
-      ulEl.innerHTML = '활성화 상태입니다.'
-      btnEl.alt = "펼치기"
-      btnEl.style.transform = 'rotateX(180deg)'
-      summaryEl.style.display = 'none'
+      btnEl.alt = "펼치기";
+      btnEl.style.transform = 'rotateX(180deg)';
+      summaryEl.style.display = 'none';
+      // localStorage 아이템 Get !
+      const json = localStorage.getItem('cartProduct');
+      let cartProduct = JSON.parse(json);
+      console.log(cartProduct)
+      // DOM Create & Render !
+      const fragmentEl = document.createDocumentFragment();
+      cartProduct.forEach(product => {
+        const { title, price, quantity, thumbnailImage } = product;
+        const liEl = makeDOMwithProperties('li', { className : "item" })
+        const imgEl = makeDOMwithProperties('img', { "src" : thumbnailImage, "className" : "thumbnail" })
+        liEl.append(imgEl);
+        liEl.innerHTML += /* html */`
+        <span class="title-wrapper">
+            <span class="title">${title}</span>
+          </span>
+          <div class="quantity-wrapper">
+            <span class="quantity">${quantity}</span>
+            <span>개</span>
+          </div>
+          <span class="price-wrapper">
+            <span class="price">${price}</span>
+            <span>원</span>
+          </span>
+        `
+        fragmentEl.append(liEl);
+      })
+      console.log(fragmentEl)
+      ulEl.append(fragmentEl);
     }
     if(isActive) {
       ulEl.innerHTML = '';
-      btnEl.alt = "펼치기"
-      btnEl.style.transform = 'rotateX(0)'
-      summaryEl.style.display = 'block'
+      btnEl.alt = "펼치기";
+      btnEl.style.transform = 'rotateX(0)';
+      summaryEl.style.display = 'block';
     }
     isActive = !isActive;
-
-    // localStorage 아이템 Get !
-    // DOM Create !
-    // -> Render DOM Element !
   }
 }
 
 function toggleCouponList() {
-  const couponListEl = $('.coupon-list')
-  couponListEl.classList.toggle('opened')
+  const couponListEl = $('.coupon-list');
+  couponListEl.classList.toggle('opened');
+}
+
+function createProductListEl() {
+
 }
