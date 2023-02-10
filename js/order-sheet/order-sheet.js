709e0fefd274b908ff8c32b1b0bc04799d149367
bongsee <ajinsuk3@naver.com>
2023-02-10 13:43:58 +0900

    :recycle: DOM render 방식 변경
    template.content 활용.

--- js/order-sheet/order-sheet.js
+++ js/order-sheet/order-sheet.js
@@ -1,7 +1,3 @@
-// import { makeDOMwithProperties } from "../utils/dom.js"
-
-import { makeDOMwithProperties } from '../utils/dom';
-
 /* COMMON */
 const $ = selector => document.querySelector(selector)
 
@@ -16,6 +12,8 @@ toggleCouponListEl.addEventListener('click', toggleCouponList);
 
 /* GLOBAL LOGIC */
 setMockData();
+
+// 주문 상품 섹션 초기 렌더링 작업 (Summary)
 
 
 
@@ -43,45 +41,51 @@ function toggleOrderList() {
     const btnEl = event.currentTarget;
     const ulEl = $('.order-list-area > ul');
     const summaryEl = $('.order-list-area > .summary');
+
+    // localStorage 아이템 Get !
+    const json = localStorage.getItem('cartProduct');
+    let cartProduct = JSON.parse(json);
+
+    // close -> open
     if(!isActive) {
-      btnEl.alt = "펼치기";
+      btnEl.alt = "닫기";
       btnEl.style.transform = 'rotateX(180deg)';
       summaryEl.style.display = 'none';
-      // localStorage 아이템 Get !
-      const json = localStorage.getItem('cartProduct');
-      let cartProduct = JSON.parse(json);
-      console.log(cartProduct)
+      
       // DOM Create & Render !
-      const fragmentEl = document.createDocumentFragment();
+      const templateEl = document.createElement('template');
       cartProduct.forEach(product => {
         const { title, price, quantity, thumbnailImage } = product;
-        const liEl = makeDOMwithProperties('li', { className : "item" })
-        const imgEl = makeDOMwithProperties('img', { "src" : thumbnailImage, "className" : "thumbnail" })
-        liEl.append(imgEl);
-        liEl.innerHTML += /* html */`
-        <span class="title-wrapper">
-            <span class="title">${title}</span>
-          </span>
-          <div class="quantity-wrapper">
-            <span class="quantity">${quantity}</span>
-            <span>개</span>
-          </div>
-          <span class="price-wrapper">
-            <span class="price">${price}</span>
-            <span>원</span>
-          </span>
+        templateEl.innerHTML += /* html */`
+          <li class="item">
+            <img class="thumbnail" src=${thumbnailImage}></img>
+            <span class="title-wrapper">
+              <span class="title">${title}</span>
+            </span>
+            <div class="quantity-wrapper">
+              <span class="quantity">${quantity}</span>
+              <span>개</span>
+            </div>
+            <span class="price-wrapper">
+              <span class="price">${price}</span>
+              <span>원</span>
+            </span>
+          </li>
         `
-        fragmentEl.append(liEl);
       })
-      console.log(fragmentEl)
-      ulEl.append(fragmentEl);
+      ulEl.append(templateEl.content);
     }
+
+    // open -> close
     if(isActive) {
       ulEl.innerHTML = '';
       btnEl.alt = "펼치기";
       btnEl.style.transform = 'rotateX(0)';
       summaryEl.style.display = 'block';
+      summaryEl
     }
+
+    // toggle state of flag variable
     isActive = !isActive;
   }
 }
