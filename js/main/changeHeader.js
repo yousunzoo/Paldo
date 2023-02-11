9e8c37f5501c8f79d8848cfd341019dadbb3ba87
yousunzoo <102499959+yousunzoo@users.noreply.github.com>
2023-02-11 20:03:00 +0900

    :sparkles: header menu 변경 방법 바꿈
    class 사용해서 제어. 추후 SPA 구현 시에 원래 코드 사용

--- js/main/changeHeader.js
+++ js/main/changeHeader.js
@@ -2,14 +2,18 @@ import { requestLogout } from "../api/requestLogout";
 import { makeDOMwithProperties } from "../utils/dom";
 
 export function changeHeader() {
-  const headerMenu = document.querySelector(".header-menu");
-  headerMenu.innerHTML = "";
+  // 로그인 한 유저 userName 가져오기
   const loginId = JSON.parse(localStorage.getItem("loginInfo")).loginId;
   const userName = JSON.parse(localStorage.getItem(loginId)).userInfo
     .displayName;
+  /* SPA 구현 시 사용
+     // 로그인 시 headerMenu 아이템 바뀜
+  const headerMenu = document.querySelector(".header-menu");
+  headerMenu.innerHTML = "";
+
   const userMenu = makeDOMwithProperties("li", {
     className: "header-menu-item",
-    textContent: `${userName}님 환영합니다!`,
+    innerHTML: `<a href="./order-list.html">${userName}님 환영합니다!</a>`,
   });
   const logoutMenu = makeDOMwithProperties("li", {
     className: "header-menu-item",
@@ -21,7 +25,19 @@ export function changeHeader() {
   });
   logoutMenu.append(logoutButton);
   headerMenu.append(userMenu, logoutMenu);
+   */
 
+  // user-menu class active 부여
+  const loginMenu = document.querySelector(".login-menu");
+  const userMenu = document.querySelector(".user-menu");
+  const userNameSpan = userMenu.querySelector(".user-name");
+  userNameSpan.textContent = userName;
+
+  loginMenu.classList.remove("active");
+  userMenu.classList.add("active");
+  const logoutButton = userMenu.querySelector(".logout-button");
+
+  // 로그아웃 버튼 클릭 시 로그아웃
   logoutButton.addEventListener("click", async function () {
     const logout = await requestLogout();
     if (logout) location.reload();
