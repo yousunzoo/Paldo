57eda93e73642f64d667b89776ad222cdbe1c5b4
yousunzoo <102499959+yousunzoo@users.noreply.github.com>
2023-02-12 19:57:40 +0900

    :tada: 썸네일 유효성 검사
    파일 크기 1MB 이하
    이미지 파일만 업로드 가능
    에러 메세지는 html에 세팅해놓고 class로 제어하도록 바꿈

--- js/signup/signup.js
+++ js/signup/signup.js
@@ -36,40 +36,29 @@ signupButton.addEventListener("click", (event) => {
   }
 });
 
-// 유효성 오류 시 보여질 메세지 span
-const errorMessageSpan = makeDOMwithProperties("span", {
-  className: "error-message",
-});
-
 // 이메일 유효성 검사
-emailInput.addEventListener("keyup", (event) => {
+emailInput.addEventListener("input", (event) => {
   return validEmailCheck(event.target.value);
 });
 
 // 패스워드 유효성 검사
-passwordInput.addEventListener("keyup", (event) => {
+passwordInput.addEventListener("input", (event) => {
   validpassWordCheck(event.target.value);
 });
 
 // 패스워드 확인 유효성 검사
-passwordCheckInput.addEventListener("keyup", (event) => {
+passwordCheckInput.addEventListener("input", (event) => {
   samePasswordCheck(event.target.value);
 });
 // 사용자 이름 유효성 검사
-userNameInput.addEventListener("keyup", (event) => {
+userNameInput.addEventListener("input", (event) => {
   validUserNameCheck(event.target.value);
 });
 
 // 썸네일 파일 업로드 시 썸네일 보이도록
 thumbnailInput.addEventListener("change", () => {
   const file = thumbnailInput.files[0];
-  const reader = new FileReader();
-  reader.readAsDataURL(file);
-  const profileImgBase64 = reader.addEventListener("load", (e) => {
-    const profileImgBase64 = e.target.result;
-    thumbnailFigure.innerHTML = `<img src=${profileImgBase64} alt="선택한 사진" />`;
-    return profileImgBase64;
-  });
+  validThumbnailCheck(file);
 });
 
 function validEmailCheck(email) {
@@ -77,13 +66,19 @@ function validEmailCheck(email) {
     /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
   if (email.match(pattern) != null) {
     if (emailInput.parentElement.querySelector(".error-message")) {
-      emailInput.parentElement.removeChild(errorMessageSpan);
+      // emailInput.parentElement.removeChild(errorMessageSpan);
+
+      emailInput.parentElement
+        .querySelector(".error-message")
+        .classList.remove("active");
       emailInput.classList.remove("error");
       validCheck.email = true;
     }
   } else {
-    errorMessageSpan.textContent = "올바른 이메일 형식을 입력해주세요";
-    emailInput.parentElement.append(errorMessageSpan);
+    // errorMessageSpan.textContent = "올바른 이메일 형식을 입력해주세요";
+    emailInput.parentElement
+      .querySelector(".error-message")
+      .classList.add("active");
     emailInput.classList.add("error");
     validCheck.email = false;
   }
@@ -92,26 +87,29 @@ function validEmailCheck(email) {
 function validpassWordCheck(password) {
   const num = password.search(/[0-9]/g);
   const eng = password.search(/[a-z]/gi);
-
+  const passwordErrorMesssage =
+    passwordInput.parentElement.querySelector(".error-message");
   if (password.length < 8 || password.length > 20) {
-    errorMessageSpan.textContent =
+    passwordErrorMesssage.textContent =
       "비밀번호를 8자리 이상 20자리 이하로 입력해주세요.";
-    passwordInput.parentElement.append(errorMessageSpan);
+    passwordErrorMesssage.classList.add("active");
     passwordInput.classList.add("error");
     validCheck.password = false;
   } else if (password.search(/\s/) != -1) {
-    errorMessageSpan.textContent = "비밀번호는 공백 없이 입력해주세요.";
-    passwordInput.parentElement.append(errorMessageSpan);
+    passwordErrorMesssage.textContent = "비밀번호는 공백 없이 입력해주세요.";
+    !passwordErrorMesssage.classList.contains("active") &&
+      passwordErrorMesssage.classList.add("active");
     passwordInput.classList.add("error");
     validCheck.password = false;
   } else if (num < 0 || eng < 0) {
-    errorMessageSpan.textContent = "영문,숫자를 혼합하여 입력해주세요.";
-    passwordInput.parentElement.append(errorMessageSpan);
+    passwordErrorMesssage.textContent = "영문,숫자를 혼합하여 입력해주세요.";
+    !passwordErrorMesssage.classList.contains("active") &&
+      passwordErrorMesssage.classList.add("active");
     passwordInput.classList.add("error");
     validCheck.password = false;
   } else {
     if (passwordInput.parentElement.querySelector(".error-message")) {
-      passwordInput.parentElement.removeChild(errorMessageSpan);
+      passwordErrorMesssage.classList.remove("active");
       passwordInput.classList.remove("error");
       validCheck.password = true;
     }
@@ -120,14 +118,16 @@ function validpassWordCheck(password) {
 
 function samePasswordCheck(password) {
   const enteredPassword = passwordInput.value;
+  const samePasswordCheckSpan =
+    passwordCheckInput.parentElement.querySelector(".error-message");
+
   if (password != enteredPassword) {
-    errorMessageSpan.textContent = "비밀번호가 일치하지 않습니다.";
-    passwordCheckInput.parentElement.append(errorMessageSpan);
+    samePasswordCheckSpan.classList.add("active");
     passwordCheckInput.classList.add("error");
     validCheck.passwordCheck = false;
   } else {
-    if (passwordCheckInput.parentElement.querySelector(".error-message")) {
-      passwordCheckInput.parentElement.removeChild(errorMessageSpan);
+    if (samePasswordCheckSpan.classList.contains("active")) {
+      samePasswordCheckSpan.classList.remove("active");
       passwordCheckInput.classList.remove("error");
     }
 
@@ -136,16 +136,50 @@ function samePasswordCheck(password) {
 }
 
 function validUserNameCheck(userName) {
+  const errorMessageSpan =
+    userNameInput.parentElement.querySelector(".error-message");
   if (userName.length > 20) {
-    errorMessageSpan.textContent = "사용자 이름은 20자 이하여야 합니다.";
-    userNameInput.parentElement.append(errorMessageSpan);
+    errorMessageSpan.classList.add("active");
     userNameInput.classList.add("error");
     validCheck.displayName = false;
   } else {
-    if (userNameInput.parentElement.querySelector(".error-message")) {
-      userNameInput.parentElement.removeChild(errorMessageSpan);
+    if (errorMessageSpan.classList.contains("active")) {
+      errorMessageSpan.classList.remove("active");
       userNameInput.classList.remove("error");
     }
     validCheck.displayName = true;
+  }
+}
+
+function validThumbnailCheck(file) {
+  const MAXIMUM_SIZE = 1000000;
+  const validFileType = [
+    "image/jpg",
+    "image/jpeg",
+    "image/webp",
+    "image/png",
+    "image/gif",
+    "image/svg+xml",
+  ];
+  const errorMessageSpan =
+    thumbnailInput.parentElement.querySelector(".error-message");
+  console.log(file.type);
+  // 파일 크기 1MB 이하
+  if (file.size > MAXIMUM_SIZE) {
+    errorMessageSpan.textContent = "파일 크기는 1MB 이하여야 합니다.";
+    errorMessageSpan.classList.add("active");
+    return;
+  } else if (!validFileType.includes(file.type)) {
+    errorMessageSpan.textContent = "유효한 파일 형식이 아닙니다.";
+    !errorMessageSpan.classList.contains("active") &&
+      errorMessageSpan.classList.add("active");
+  } else {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.addEventListener("load", (e) => {
+      const profileImgBase64 = e.target.result;
+      thumbnailFigure.innerHTML = `<img src=${profileImgBase64} alt="선택한 사진" />`;
+      return profileImgBase64;
+    });
   }
 }
