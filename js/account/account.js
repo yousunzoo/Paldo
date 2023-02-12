62f4d4a247bebcec6cea0b4f127f842af6457704
bongsee <ajinsuk3@naver.com>
2023-02-12 17:00:57 +0900

    :sparkles: 계좌 목록 조회, 계좌 연결 API 연동

--- js/account/account.js
+++ js/account/account.js
@@ -1,12 +1,17 @@
-import { getBankList } from './accountApi.js';
+import { getBankList, connectBankAccount } from './accountApi.js';
 
 /* COMMON */
 const $ = selector => document.querySelector(selector)
 
 /* DOM */
 const modalTrigger = $('.add-account-btn');
+const accountFormEl = $('#accountForm');
 
 /* EVENT HANDLER */
+
+/**
+ * 계좌 추가를 위한 모달창 작업
+ */
 modalTrigger.addEventListener('click', async () => {
   // localStroage Get !
   const json = localStorage.getItem('loginInfo');
@@ -16,11 +21,10 @@ modalTrigger.addEventListener('click', async () => {
   // 계좌 목록 조회 API !
   let bankList = await getBankList(accessToken);
 
-  // DOM Create & Render !
+  // DOM Create !
   bankList = bankList.filter(bank => {
-    return bank.name !== "케이뱅크"
+    return bank.name !== "케이뱅크" // 케이뱅크는 목록에서 제외합니다.
   });
-  console.log(bankList)
 
   const templateEl = document.createElement('template');
   bankList.forEach(bank => {
@@ -28,15 +32,76 @@ modalTrigger.addEventListener('click', async () => {
     const digitsFormat = String(digits).replaceAll(',', '-') // 계좌 번호 간 구분 기호로 좀 더 친숙한 하이픈으로 교체합니다.
     templateEl.innerHTML += /* html */`
       <li class="item">
-        <input type="radio" name="bank" id=${code}>
+        <input type="radio" name="bank" id=${code} required>
         <label for=${code}>${name} [${digitsFormat}]</label>
       </li>
     `
   })
+  // Render !
   const ulEl = $('.bank-list');
   ulEl.innerHTML = '';
   ulEl.append(templateEl.content)
+
+  // 이벤트 핸들러 : 계좌 자리수 추출을 위한 select change
+  let totalDigits;
+  ulEl.addEventListener('change', getTotalAccountDigits)
+
+  function getTotalAccountDigits(event) {
+    if(event.target.matches('input[type="radio"]')) {
+      const str = event.target.labels[0].innerText;
+      const matches = str.match(/\d+/g) || [];
+      totalDigits = matches.reduce((acc,cur) => {
+        return acc + Number(cur);
+      }, 0)
+    }
+  }
+
+  // 이벤트 핸들러 : 폼 submit
+  accountFormEl.addEventListener('submit', submitAccountForm)
+
+  function submitAccountForm(event) {
+    event.preventDefault();
+
+    // 선택한 은행 코드 추출
+    const selectedBank = Array.from(event.target).find(input => input.checked);
+    const bankCode = selectedBank.id;
+
+    // 계좌 번호, 전화 번호 추출
+    const accountNumber = event.target["accountNumber"].value;
+    const phoneNumber = event.target["phoneNumber"].value;
+
+    // 계좌 번호 자리수 유효성 검사
+    if(totalDigits !== accountNumber.length) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: '계좌 번호 자리수를 확인하세요.',
+      })
+      return;
+    }
+
+    const submitData = { bankCode, accountNumber, phoneNumber, signature : true }
+    // 계좌 연결 API
+    try {
+      connectBankAccount(accessToken, submitData)
+    } catch(err) {
+      console.error(err);
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: '계좌 등록에 실패했습니다.',
+      })
+    }
+
+    const modalEl = $('input[type="checkbox"]#modal');
+    modalEl.checked = false; // 모달창 닫기
+
+    // form 제출 완료되면 내부에서 사용한 이벤트 핸들러 제거
+    ulEl.removeEventListener('change', getTotalAccountDigits)
+    accountFormEl.removeEventListener('submit', submitAccountForm)
+  }
 })
+
 
 
 /* GLOBAL LOGIC */
--- js/account/accountApi.js
+++ js/account/accountApi.js
@@ -1,11 +1,37 @@
 import { headers, url } from '../api/headers.js'
 
+// 모든 API는 외부에서 accessToken을 받아 API 요청을 보냅니다.
+
+/**
+ * 계좌 목록 조회
+ * @param {*} accessToken 
+ * @returns 응답 json 
+ */
 export async function getBankList (accessToken) {
   const res = await fetch(`${url}account/banks`, {
     headers : {
       ...headers,
       Authorization: `Bearer ${accessToken}`
     }
+  })
+  const json = await res.json();
+  return json;
+}
+
+/**
+ * 계좌 연결
+ * @param {*} accessToken 
+ * @param {*} body 
+ * @returns 응답 json
+ */
+export async function connectBankAccount (accessToken, body) {
+  const res = await fetch(`${url}account`, {
+    method: 'POST',
+    headers : {
+      ...headers,
+      Authorization: `Bearer ${accessToken}`
+    },
+    body: JSON.stringify(body)
   })
   const json = await res.json();
   return json;
