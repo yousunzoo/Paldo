1e5b1eced204215eec5c8697dc88ad865b2486d4
kjun <kjun_all@naver.com>
2023-02-13 23:22:52 +0900

    :sparkles: 권범준 admin 페이지네이션 작업

---
+++ js/admin_goodsList/pagination.js
@@ -0,0 +1,113 @@
+import { getProduct } from "../api/getProduct";
+import { renderGoodsList, ulEl } from "./renderGoodsList";
+
+export let isRendered = false;
+
+const paginationEl = document.querySelector(".pagination");
+let limit = 10;
+let currentPage = 1;
+let tagList = ["초콜릿/캔디류", "라면", "음료", "스낵", "인기", "세일", "신상"];
+let boolean = ["YES", "NO"];
+export const pagination = async (search = undefined) => {
+  let listEls = await getProduct();
+  let arr;
+  let totalCount;
+  if (tagList.includes(search)) {
+    arr = filterByTag(listEls, search);
+    totalCount = arr.length * 10;
+  } else if (boolean.includes(search)) {
+    arr = filterByBoolean(listEls, search);
+    totalCount = arr.length * 10;
+  } else if (search === undefined) {
+    arr = filterAll(listEls);
+    totalCount = listEls.length;
+  } else {
+    arr = filterBySearch(listEls, search);
+    totalCount = arr.length * 10;
+  }
+
+  let totalPage = Math.ceil(totalCount / limit);
+
+  // 첫번째 숫자는 마지막 숫자에서 전체 카운트 - 1
+
+  let html = "";
+
+  for (let i = 1; i <= totalPage; i += 1) {
+    html += `<button class="pageNumber" id="page_${i}">${i}</button>`;
+  }
+
+  paginationEl.innerHTML = html;
+
+  const currentPageNums = document.querySelectorAll(".pagination button");
+  currentPageNums.forEach((button) => {
+    button.addEventListener("click", () => {
+      currentPage = button.innerText;
+      ulEl.innerHTML = "";
+      renderGoodsList(arr[currentPage - 1]);
+    });
+  });
+
+  if (isRendered === false) {
+    renderGoodsList(arr[currentPage - 1]);
+    isRendered = true;
+  }
+};
+
+// 카테고리 필터링
+export const filterByTag = (listEls, search) => {
+  const searchFilters = listEls.filter((item) => item.tags.includes(search));
+  const arr = Array(Math.ceil(searchFilters.length / 10)).fill([]);
+  let num = 0;
+  for (let i in arr) {
+    arr[i] = searchFilters.slice(num, num + 10);
+    num += 10;
+    ulEl.innerHTML = "";
+    isRendered = false;
+  }
+  return arr;
+};
+
+// 품절여부 필터링
+export const filterByBoolean = (listEls, search) => {
+  let yesOrNo;
+  if (search === "YES") {
+    yesOrNo = true;
+  } else {
+    yesOrNo = false;
+  }
+  const searchFilters = listEls.filter((item) => item.isSoldOut === yesOrNo);
+  const arr = Array(Math.ceil(searchFilters.length / 10)).fill([]);
+  let num = 0;
+  for (let i in arr) {
+    arr[i] = searchFilters.slice(num, num + 10);
+    num += 10;
+    ulEl.innerHTML = "";
+    isRendered = false;
+  }
+  return arr;
+};
+
+// 검색어 필터링
+export const filterBySearch = (listEls, search) => {
+  const searchFilters = listEls.filter((item) => item.title.includes(search));
+  const arr = Array(Math.ceil(searchFilters.length / 10)).fill([]);
+  let num = 0;
+  for (let i in arr) {
+    arr[i] = searchFilters.slice(num, num + 10);
+    num += 10;
+    ulEl.innerHTML = "";
+    isRendered = false;
+  }
+  return arr;
+};
+
+// 전체목록
+export const filterAll = (listEls) => {
+  const arr = Array(Math.ceil(listEls.length / 10)).fill([]);
+  let num = 0;
+  for (let i in arr) {
+    arr[i] = listEls.slice(num, num + 10);
+    num += 10;
+  }
+  return arr;
+};
--- js/main.js
+++ js/main.js
@@ -1,5 +1,58 @@
+// import { pagination } from "./admin_goodsList/pagination";
+import { pagination } from "./admin_goodsList/pagination";
+import { ulEl } from "./admin_goodsList/renderGoodsList";
 import { checkAuthorization } from "./api/checkAuthorization";
 window.onload = async function () {
   const isLogin = await checkAuthorization();
-  console.log(isLogin);
 };
+
+const categoryBtn = document.querySelector(".btn-category");
+const categoryList = document.querySelector(".category-list");
+const soldoutBtn = document.querySelector(".btn-soldout");
+const soldoutList = document.querySelector(".soldout-list");
+
+const toggleClass = (btn, list) => {
+  btn.addEventListener("click", () => {
+    list.classList.contains("active") ? list.classList.remove("active") : list.classList.add("active");
+  });
+  btn.addEventListener("focusout", () => {
+    list.classList.remove("active");
+  });
+};
+
+toggleClass(categoryBtn, categoryList);
+toggleClass(soldoutBtn, soldoutList);
+
+window.addEventListener("click", (e) => {
+  if (e.target.className !== "list-tag") return;
+  ulEl.innerHTML = "";
+  pagination(e.target.textContent);
+});
+
+let search;
+pagination(search);
+
+// 검색 버튼 입력 했을때
+const inputEl = document.querySelector(".search-goodsname");
+const inputBtnEl = document.querySelector(".search");
+inputEl.addEventListener("keydown", () => {
+  if (event.key === "Enter" && !event.isComposing) {
+    search = inputEl.value;
+    pagination(search);
+  }
+});
+
+inputBtnEl.addEventListener("click", (e) => {
+  search = inputEl.value;
+  pagination(search);
+});
+
+const allCheck = document.querySelector(".all-check input");
+const itemCheck = document.querySelectorAll(".item-check input");
+console.log(itemCheck);
+
+window.addEventListener("DOMContentLoaded", (e) => {
+  allCheck.addEventListener("click", (e) => {
+    console.log(itemCheck);
+  });
+});
