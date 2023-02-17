68374366eec5a7f8dbd475e5d08db8260cb79c70
yousunzoo <102499959+yousunzoo@users.noreply.github.com>
2023-02-17 18:29:02 +0900

    :lipstick: 각 상품 페이지 배너 추가

--- js/product/setProductPage.js
+++ js/product/setProductPage.js
@@ -1,29 +1,38 @@
-// localStorage에서 검색어 불러와서 실행
-
+import bannerNew from "../../static/images/productBanner-new.png";
+import bannerBest from "../../static/images/productBanner-best.png";
+import bannerFrugal from "../../static/images/productBanner-frugal.png";
 import { getProducts } from "../api/getProducts";
 
 export default async function setProductPage(tag) {
+  // 타이틀 세팅
+  const keywordTitle = document.querySelector(".product-title");
+  const productBanner = document.querySelector(".product-banner");
+  let url;
+  let title;
+  switch (tag) {
+    case "new":
+      title = "신상품";
+      url = bannerNew;
+      break;
+    case "best":
+      title = "베스트";
+      url = bannerBest;
+      break;
+    case "frugal":
+      title = "알뜰쇼핑";
+      url = bannerFrugal;
+  }
+  keywordTitle.textContent = title;
+  productBanner.style.backgroundImage = `url(${url})`;
+
+  // 상품리스트 세팅
   const searchResult = await getProducts("", tag);
   const originResult = [...searchResult];
   showResult(searchResult, originResult, tag);
 }
 
 function showResult(searchResult, originResult, tag) {
-  const keywordTitle = document.querySelector(".product-title");
   const amount = document.querySelector(".count");
-  let title;
-  switch (tag) {
-    case "new":
-      title = "신상품";
-      break;
-    case "best":
-      title = "베스트";
-      break;
-    case "frugal":
-      title = "알뜰쇼핑";
-  }
-  keywordTitle.textContent = title;
-
   amount.textContent = searchResult.length;
   setProductList(searchResult, "최신순", originResult);
   changeTabs(searchResult, originResult);
