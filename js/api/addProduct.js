32ee546557249d23e57851bddecc9532f13ffc4a
kjun <kjun_all@naver.com>
2023-02-22 16:37:34 +0900

    :lipstick: admin페이지 api 모듈 수정
    addProduct, getProduct, getSingleProduct

--- js/api/addProduct.js
+++ js/api/addProduct.js
@@ -4,6 +4,7 @@ const url = "https://asia-northeast3-heropy-api.cloudfunctions.net/api/products"
 
 export const addProduct = async (payload) => {
   const { title, price, description, tags, thumbnailBase64, photoBase64, discountRate } = payload;
+  console.log(price);
   const res = await fetch(url, {
     method: "POST",
     headers,
--- js/api/getProduct.js
+++ js/api/getProduct.js
@@ -2,6 +2,16 @@ import { headers } from "./headers";
 
 const url = "https://asia-northeast3-heropy-api.cloudfunctions.net/api/products";
 
+export const memoizedGetProduct = (() => {
+  let product;
+  return async () => {
+    if (!product) {
+      product = await getProduct();
+    }
+    return product;
+  };
+})();
+
 export const getProduct = async () => {
   try {
     const res = await fetch(url, {
