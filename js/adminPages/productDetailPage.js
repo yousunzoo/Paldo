5f5c31880b2bbb8b2c0dc43a955b8e4ac1bf9d89
kjun <kjun_all@naver.com>
2023-02-22 16:25:27 +0900

    :sparkles: admin페이지 페이지별 HTML요소 파일
    addProductPage, detailTransactionPage, productDetailPage, productEditPage, productPage, reportPage, transactionPage

---
+++ js/adminPages/productDetailPage.js
@@ -0,0 +1,52 @@
+export default /* html */ `
+    <div class="all-wrapper">
+      <div class="goods-title-wrapper">
+        <div class="title-header">
+          <div class="title">상품 상세페이지</div>
+        </div>
+        <div class="button-wrapper">
+          <div class="edit">
+            <a href="/product/editproduct/" class="edit-button">수정</a>
+          </div>
+          <div class="delete">
+            <a class="delete-button" href="/product" class="delete-button">삭제</a>
+          </div>
+        </div>
+      </div>
+      <div class="goods-item-wrapper">
+        <div class="bar"></div>
+        <div class="goods-info">
+          <div class="goods-info-wrapper">
+            <div class="goods-thumbnail">
+              <img src="" alt="" />
+            </div>
+            <div class="goods-info-text">
+              <div class="category">
+                <div class="title">카테고리</div>
+                <div class="val">스낵</div>
+              </div>
+              <div class="name">
+                <div class="title">상품명</div>
+                <div class="val">감자깡</div>
+              </div>
+              <div class="price">
+                <div class="title">가격</div>
+                <div class="val">50,000원</div>
+              </div>
+              <div class="sale">
+                <div class="title">할인율</div>
+                <div class="val">10%</div>
+              </div>
+              <div class="tag">
+                <div class="title">태그</div>
+                <div class="val">인기</div>
+              </div>
+            </div>
+          </div>
+          <div class="bar"></div>
+          <div class="detail-img">
+            <img src="" alt="" />
+          </div>
+        </div>
+      </div>
+    </div>`;
