8dfb13c5cfde1b8c1edfaf95d8678bc01af3cd92
bongsee <ajinsuk3@naver.com>
2023-02-26 02:51:03 +0900

    :recycle: 주문내역 페이지 로직 수정
    - 거래 시간 차이가 5초 내인 거래목록을 그룹핑하여 렌더링
    - 같은 제품에 대한 여러 거래 요청 건을 일괄 주문 취소/거래 확정되도록 수정

--- js/userOrderList/orderListApi.js
+++ js/userOrderList/orderListApi.js
@@ -26,21 +26,20 @@ export async function getOrderList() {
 
 export async function cancelTransaction(body) {
   const accessToken = getLocalStorageData(ACCESS_TOKEN);
-  const res = fetch(`${url}products/cancel`, {
-    method: 'POST',
-    headers : {
-      ...headers,
-      Authorization: `Bearer ${accessToken}`
-    },
-    body: JSON.stringify(body)
-  })
-  .then((result) => {
-    return result.json();
-  })
-  .catch((err) => {
-    console.log(err)
-  })
-  return res;
+  try {
+    const res = fetch(`${url}products/cancel`, {
+      method: 'POST',
+      headers : {
+        ...headers,
+        Authorization: `Bearer ${accessToken}`
+      },
+      body: JSON.stringify(body)
+    })
+    const json = await res.json();
+    return json;
+  } catch(error) {
+    return false;
+  }
 }
 
 export async function confirmTransaction(body) {
